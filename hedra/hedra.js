45af757c34375ea53233f85940db84e41d3cbe89
Hedra Emad <emadhedra4@gmail.com>
2026-01-23 17:15:27 +0200

    link css & js fills and meal page

--- hedra/hedra.js
+++ hedra/hedra.js
@@ -4,13 +4,24 @@ async function getMeal(mealID) {
   );
 
   meal = await meal.json();
-  console.log(meal.meals[0]);
+  console.log(meal);
+  displayMeal(meal.meals[0]);
 }
 
+getMeal(53136);
+
+var row = document.getElementById("rowData");
 function displayMeal(meal) {
+  let recipes = "";
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      recipes += `<li class="my-3 mx-1 p-1 alert-success rounded">${meal[`strMeasure${i}`]} ${meal[`strIngredient${i}`]}</li>`;
+    }
+  }
+
   let html = ` <div class="col-md-4 myM text-white">
         <img class="w-100" src="${meal.strMealThumb}" alt="" srcset=""><br>
-        <h1>${meal.strMeal}</h1>
+        <h1 class="text-center mt-3">${meal.strMeal}</h1>
     </div>
     <div class="col-md-8 myM text-white text-left">
         <h2>Instructions</h2>
@@ -21,12 +32,10 @@ function displayMeal(meal) {
         <ul class="d-flex " id="recipes">
         </ul>
 
-        <h3 class="my-2 mx-1 p-1">Tags :</h3>
-        <ul class="d-flex " id="tags">
-        </ul>
-
-
         <a class="btn btn-success text-white" target="_blank" href="${meal.strSource}">Source</a>
         <a class="btn youtube text-white" target="_blank" href="${meal.strYoutube}">Youtub</a>
     </div>`;
+
+  row.innerHTML = html;
+  document.getElementById("recipes").innerHTML = recipes;
 }
