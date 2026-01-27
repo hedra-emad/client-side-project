8e6c0676bd621427eb7441ae0c261d9643e3ca94
Mohamed-Yasser <mhmmedyasser@gmail.com>
2026-01-27 14:23:53 +0200

    Add authentication check before adding meal to favorites

--- js/main.js
+++ js/main.js
@@ -29,10 +29,54 @@ function display() {
     } else collect += createMeal(meal);
   }
   mealsResult.innerHTML = collect;
+  MankeFavorite();
+}
+
+function showLoginAlert() {
+  const alertBox = document.getElementById("login-alert");
+  if (alertBox) {
+    alertBox.style.display = "block";
+    setTimeout(() => {
+      alertBox.style.display = "none";
+    }, 1000);
+  }
+}
+
+function MankeFavorite() {
+  for (let meal of meals) {
+    let FavoriteMeals = document.getElementById('fav-' + meal.idMeal);
+
+    if (FavoriteMeals) {
+      FavoriteMeals.onclick = function (e) {
+        e.preventDefault(); 
+        e.stopPropagation(); 
+
+        const isUserLoggedIn = localStorage.getItem("loggedUser");
+        if (!isUserLoggedIn) {
+          showLoginAlert();
+          return; 
+        }
+        const key = 'fav' + meal.idMeal;
+        if (localStorage.getItem(key)) {
+          localStorage.removeItem(key);
+          FavoriteMeals.classList.remove('active');
+        } else {
+          localStorage.setItem(key, meal.idMeal);
+          FavoriteMeals.classList.add('active');
+        }
+      };
+    }
+  }
 }
 
 export function createMeal(meal, highlight) {
   var regex = new RegExp(searchByNameInput.value, "i");
+  const isUserLoggedIn = localStorage.getItem("loggedUser");
+  var activeClass = "";
+  if (isUserLoggedIn) {
+  const isFav = localStorage.getItem('fav' + meal.idMeal);
+  activeClass = isFav ? 'active' : '';
+  }
   return `<a href="pages/meal.html?id=${meal.idMeal}">
   <div class="col" >
   <div class="meal-card rounded-3" >
@@ -54,7 +98,7 @@ export function createMeal(meal, highlight) {
       <p>${meal.strArea} • ${meal.strCategory}</p>
      </div>
        <div>
-       <span class="fav-icon">
+       <span class="fav-icon ${activeClass}" id="fav-${meal.idMeal}">
       <i class="fa-solid fa-heart"></i>
       </span>
        </div>
